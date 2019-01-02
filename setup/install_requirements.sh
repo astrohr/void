d2454d215af8b5d23c336368a6281f6d6a194afd
xXxGandalf420xXx <andrija.radica@gmail.com>
2019-01-02 02:58:25 +0100

    Bug fix

--- setup/install_requirements.sh
+++ setup/install_requirements.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 sudo pip3 install -r python3-requirements.txt
 sed 's/#.*//' packages.txt | xargs sudo apt-get install
+sudo add-apt-repository ppa:ubuntugis/ubuntugis-unstable
+sudo apt-get update
+sudo apt-get install postgis
