91c9f0a10fde8b29c7e2ca0cc1b2988df60b9520
Patrik Kukic <patrikkukic@gmail.com>
2019-01-03 21:40:01 +0100

    created common

---
+++ void/common.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+""" 
+Functions used in misc files. 
+"""
+
+import logging
+import docopt
+
+LOG_FORMAT = (
+    '%(levelname)-8s  %(asctime)s  %(process)-5d  %(name)-26s  %(message)s')
+
+def _configure_log(verbosity):
+    levels = {
+        '0': logging.CRITICAL,
+        '1': logging.ERROR,
+        '2': logging.WARNING,
+        '3': logging.INFO,
+        '4': logging.DEBUG,
+    }
+    if verbosity not in levels.keys():
+        raise docopt.DocoptExit(f'--verbosity not one of 0, 1, 2, 3, 4')
+    logging.basicConfig(level=levels[verbosity], format=LOG_FORMAT)
